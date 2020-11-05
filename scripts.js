b640a824fd51907d6140035f75948c81274c9e8a
NayeZen <NayeZen@gmail.com>
2020-11-05 01:29:16 -0600

    Update

---
+++ scripts.js
@@ -0,0 +1,9 @@
+//Function expression to select elements
+const selectElement = (s) => document.querySelector(s);
+
+selectElement('.open').addEventListener('click', () => {
+  selectElement('.nav-list').classList.add('active');
+});
+selectElement('.close').addEventListener('click', () => {
+  selectElement('.nav-list').classList.remove('active');
+});
